9174a22d73c754b82616e34373466e9a700a5cd4
rociosegovia <rociosegoviajazmin@gmail.com>
2024-05-20 16:26:53 -0300

    scripts

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por tu mensaje, nos pondremos en contacto contigo pronto.');
+});
