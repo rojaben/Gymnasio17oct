67c09cfa7c5ad6336b6ce86c2e6ae44d755eecf1
rociosegovia <rociosegoviajazmin@gmail.com>
2024-05-25 15:51:11 -0300

    ultimos arreglos

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('Gracias por tu mensaje, nos pondremos en contacto contigo pronto.');
 });
+
